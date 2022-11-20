8a7e8d90bbf8309c6f4bc29f98d9b499224b144d
@hematite2549 <hematite2549@gmail.com>
2022-11-20 15:00:23 +0700

    cos

--- test/script.js
+++ test/script.js
@@ -12,3 +12,14 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+const nav = document.querySelectorAll('.nav')
+
+open_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.add('visible'))
+})
+
+close_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.remove('visible'))
+})
